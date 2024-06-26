56cc13528c9076c19f284d67eb5a5441cb9c43a8
tiken14 <tiken14@live.com>
2024-06-26 14:43:15 +0500

    Корректировка декоратора с wraps

--- src/decorators.py
+++ src/decorators.py
@@ -1,4 +1,6 @@
 import os
+from functools import wraps
+
 import pandas as pd
 
 from config import DATA_DIR
@@ -8,9 +10,13 @@ class ReportSaver:
 
     @staticmethod
     def to_excel(file_name: str = 'result_{func}.xls'):
-
+        """
+        Декоратор для сохранения результатов в файл.
+        :param file_name: Имя файла в которую будет сохранён результат. По умолчанию result_ИмяФункции.xls
+        :return: Результат работы функции
+        """
         def wrapper(func):
-
+            @wraps(func)
             def inner(*args, **kwargs) -> pd.DataFrame:
 
                 result: pd.DataFrame = func(*args, **kwargs)
